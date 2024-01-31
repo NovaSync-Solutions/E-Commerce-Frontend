354625bd5aed59a1f468fe891bbab6e894d0c238
Joaquin <joaqobianchi13@gmail.com>
2024-01-31 00:45:47 -0300

    Tiulo de nuevos productos segun el figma

--- src/components/Sliders/SliderNewArrival.jsx
+++ src/components/Sliders/SliderNewArrival.jsx
@@ -3,7 +3,7 @@ import CardNewArrival from "../Cards/CardNewArrival";
 
 function NewArrival() {
   return (
-    <section className="flex-col pt-10 px-48 mb-20 container m-auto">
+    <section className="flex-col pt-10 mb-20 container m-auto">
       <div className="flex items-center mb-4">
         <div className="rounded w-2 h-12 bg-purple-600"></div>
         <h2 className="text-3xl ml-3 font-bold">New arrivals</h2>
