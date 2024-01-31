aff78e080fc3bd25a167961a98cd340e54caa648
Joaquin <joaqobianchi13@gmail.com>
2024-01-31 18:17:31 -0300

    Responsive Card Hero

--- src/components/Cards/CardHero.jsx
+++ src/components/Cards/CardHero.jsx
@@ -4,12 +4,12 @@ import React from "react";
 function CardHero() {
   return (
     <section>
-      <div className="flex justify-center  m-auto w-full min-w-[80%] px-28">
-        <div className="bg-[url('/assets/images/hero1.png')] bg-cover w-1/2 flex flex-col  justify-center px-20">
-          <h4 className="text-4xl text-white  font-bold my-4">
+      <div className="flex flex-col lg:flex-row justify-center  m-auto lg:px-28">
+        <div className="bg-[url('/assets/images/hero1.png')] bg-cover h-[500px]  lg:h-auto    w-full lg:w-1/2 flex flex-col  justify-center px-20 py-10">
+          <h4 className="text-2xl lg:text-4xl text-white  font-bold my-4">
             WE MADE YOUR EVERYDAY FASHION BETTER!
           </h4>
-          <p className="text-lg text-white font-light mb-4">
+          <p className="text-base lg:text-lg text-white font-light mb-4">
             In our journey to improve everyday fashion, euphonia presents
             EVERYDAY wear range - Comfortable & Affordable fashion 24/7
           </p>
@@ -22,8 +22,12 @@ function CardHero() {
             </Link>
           </div>
         </div>
-        <div className="w-1/2">
-          <img className="w-full" src="/assets/images/hero2.png" alt="" />
+        <div className="w-full hidden lg:block lg:h-auto  lg:w-1/2">
+          <img
+            className="w-full h-full object-cover"
+            src="/assets/images/hero2.png"
+            alt=""
+          />
         </div>
       </div>
     </section>
