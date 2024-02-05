7f4c81a7cd94c134b3cbfae60ad8daba0cc3f53f
Joaquin <joaqobianchi13@gmail.com>
2024-02-05 16:23:01 -0300

    Add btn busqueda en Navbar

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -11,12 +11,12 @@ function Navbar() {
     "px-2 py-2 rounded-xl hover:bg-purple-600 hover:text-white ease-in-out duration-300 bg-gray-200";
 
   return (
-    <header className="flex items-center w-full px-36  m-auto text-black bg-slate-100 h-24 overflow-hidden">
+    <header className="flex items-center w-full 2xl:px-36    text-black bg-slate-100 h-24 overflow-hidden">
       <nav className="flex items-center justify-between w-full">
         <Link href="/">
           <Image src="" width={20} height={20} alt="logo" />
         </Link>
-        <div className="flex items-center gap-10 text-lg">
+        <div className="hidden lg:flex items-center gap-10 text-lg">
           <Link href="" className="font-semibold">
             Shop
           </Link>
@@ -25,15 +25,15 @@ function Navbar() {
           <Link href="">Combos</Link>
           <Link href="">Joggers</Link>
         </div>
-        <div className="flex items-center gap-3 bg-gray-200 border border-gray-300 py-2 px-3 rounded-md">
+        <div className="flex items-center gap-3 hidden lg:flex bg-gray-200 border border-gray-300 py-2 px-3 rounded-md">
           <CiSearch className=" text-gray-700" />
           <input
-            className="bg-gray-200 outline-none font-medium "
+            className="bg-gray-200  outline-none font-medium "
             type="text"
             placeholder="Search"
           />
         </div>
-        <div className="flex  gap-3 jus items-center text-slate-600 text-2xl">
+        <div className="hidden lg:flex  gap-3 jus items-center text-slate-600 text-2xl">
           {/* agregar condicional si esta logueado o no */}
 
           <Link href="" className={buttonNavbar}>
